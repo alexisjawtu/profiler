1cb3f032d4568e3f12422dd7ecd15bccde486170
alexis <alexis.jawtuschenko@gmail.com>
2025-02-06 22:32:13 -0300

    Use string streams for the input

--- source/SetParameter.cpp
+++ source/SetParameter.cpp
@@ -35,11 +35,12 @@ void SetParameter::get_from_ui() {
     string str_input;
 
     cout << "Input the Thickness of the wafer (unit: microns) ";
-    cin >> user_thickness_of_inner_wafer;
-    cout << endl << endl;
+    getline(cin, str_input);
+    stringstream(str_input) >> user_thickness_of_inner_wafer;
+    cout << "\n\v";
 
     cout << "Now, please input the parameters for the profile mesh.\n"
-            "Please refer to pictures and examples in the documentation." << endl << endl;
+            "Please refer to pictures and examples in the documentation.\n\n";
 
     array<const char*, 3> parameter_names ({"Ceiling", "Floor", "Width"});
 
